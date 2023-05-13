aaa3efc00d545bec93fd7d560ae24367bbd757e4
KaoruMizugaki <k021g1249@g.neec.ac.jp>
2023-05-13 16:51:57 +0900

    頭痛が痛い

--- Dxlib/SpringBlock.cpp
+++ Dxlib/SpringBlock.cpp
@@ -1,56 +1,71 @@
 #include "SpringBlock.h"
 
 SpringBlock::SpringBlock(const Vector2& pos, const Vector2& offset, const Vector2& radius) :
-    IBlock(pos, offset, radius)
+	IBlock(pos, offset, radius)
 {
-    SetType(Type::SPRING);
+	SetType(Type::SPRING);
 }
 
 void SpringBlock::Update(void)
 {
+	switch (GetRotate() / 90)
+	{
+	case 0:
+
+		break;
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	}
 }
 
 void SpringBlock::Draw(void)
 {
-    int posX{ static_cast<int>(GetPos().x) }, posY{ static_cast<int>(GetPos().y) };
-    int radiusX{ static_cast<int>(GetRadius().x) }, radiusY{ static_cast<int>(GetRadius().y) };
+	int posX{ static_cast<int>(GetPos().x) }, posY{ static_cast<int>(GetPos().y) };
+	int radiusX{ static_cast<int>(GetRadius().x) }, radiusY{ static_cast<int>(GetRadius().y) };
 
-    DrawFormatString(0, 100, GetColor(200, 0, 0), "%d", GetRotate());
+	DrawFormatString(0, 100, GetColor(200, 0, 0), "%d", GetRotate());
 
-    switch (GetRotate() / 90)
-    {
-    case 0:
-        DrawTriangle(
-            posX, posY - radiusY, // でっぱり
-            posX - radiusX, posY + radiusY, // 左下
-            posX + radiusX, posY + radiusY, // 右下
-            0x00f000, true
-        );
-        break;
-    case 1:
-        DrawTriangle(
-            posX + radiusX, posY, // でっぱり
-            posX - radiusX, posY - radiusY, // 左下
-            posX - radiusX, posY + radiusY, // 右下
-            0xf00000, true
-        );
-        break;
-    case 2:
-        DrawTriangle(
-            posX, posY + radiusY, // でっぱり
-            posX - radiusX, posY - radiusY, // 左下
-            posX + radiusX, posY - radiusY, // 右下
-            0x0000f0, true
-        );
-        break;
-    case 3:
-        DrawTriangle(
-            posX - radiusX, posY, // でっぱり
-            posX + radiusX, posY - radiusY, // 左下
-            posX + radiusX, posY + radiusY, // 右下
-            0xf0f0f0, true
-        );
-        break;
-    }
+	switch (GetRotate() / 90)
+	{
+	case 0:
+		DrawTriangle(
+			posX, posY - radiusY, // でっぱり
+			posX - radiusX, posY + radiusY, // 左下
+			posX + radiusX, posY + radiusY, // 右下
+			0x00f000, true
+		);
+		break;
+	case 1:
+		DrawTriangle(
+			posX + radiusX, posY, // でっぱり
+			posX - radiusX, posY - radiusY, // 左下
+			posX - radiusX, posY + radiusY, // 右下
+			0xf00000, true
+		);
+		break;
+	case 2:
+		DrawTriangle(
+			posX, posY + radiusY, // でっぱり
+			posX - radiusX, posY - radiusY, // 左下
+			posX + radiusX, posY - radiusY, // 右下
+			0x0000f0, true
+		);
+		break;
+	case 3:
+		DrawTriangle(
+			posX - radiusX, posY, // でっぱり
+			posX + radiusX, posY - radiusY, // 左下
+			posX + radiusX, posY + radiusY, // 右下
+			0xf0f0f0, true
+		);
+		break;
+	}
 
 }
